91f3e371e5e6e3cf0c641b780b7d9e45ae130111
JacksonKrizan <72949242+JacksonKrizan@users.noreply.github.com>
2024-10-17 11:51:31 -0400

    School test Github

---
+++ Assets/Scripts/Game/Player/Gun.cs
@@ -0,0 +1,81 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering.Universal;
+using UnityEngine.UI;
+//video 2D Shooting in Unity (Tutorial)
+public class Gun : MonoBehaviour
+{
+    public Transform bulletSpawnPoint;
+    public GameObject bulletPrefab;
+    
+    public float bulletSpeed = 10;
+    public bool bulletSpawn = true;
+    public float bulletSpawnDelay;
+    public Light2D Light;
+    public Light2D Light2;
+    public Light2D Light3;
+    public bool ammo;
+    public int bulletCount;
+    public Text txtBullets;
+    void Start()
+    {
+        Light.intensity = 0f;
+        Light2.intensity = 0f;
+        Light3.intensity = 0f;
+
+    }
+
+    void Update()
+    {
+        txtBullets.text = "Bulets : " + bulletCount.ToString("F");
+
+        if (ammo == true)
+        {
+            if (bulletSpawn == true)
+            {
+                if (Input.GetKey(KeyCode.Space))
+                {
+                    Light.intensity = 100f;
+                    Light2.intensity = 10f;
+                    Light3.intensity = 2f;
+
+                    var bullet = Instantiate(bulletPrefab, bulletSpawnPoint.position, bulletSpawnPoint.rotation);
+                    bullet.GetComponent<Rigidbody2D>().velocity = bulletSpawnPoint.up * bulletSpeed;
+                    bulletSpawn = false;
+                    bulletCount -= 1;
+                    StartCoroutine(ShootBullets());
+                }
+
+            }
+
+        }
+        
+        if (bulletCount == 0)
+        {
+            ammo = false;
+
+        }
+        if (bulletCount >= 0)
+        {
+            ammo = true;
+
+        }
+    }
+
+    
+    IEnumerator ShootBullets()
+    {
+        if (bulletSpawn == false)
+        {
+            yield return new WaitForSeconds(.03f);
+            Light.intensity = 0f;
+            Light2.intensity = 0f;
+            Light3.intensity = 0f;
+            yield return new WaitForSeconds(bulletSpawnDelay);
+            bulletSpawn = true;
+            Debug.Log("pew pew");
+        }
+
+    }
+}
---
+++ Assets/Scripts/Game/Player/PlayerScript.cs
@@ -0,0 +1,124 @@
+
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class PlayerScript : MonoBehaviour
+{
+    //3 min SCORE & UI - How to make a Video Game in Unity (E07)
+
+    public float RunSpeed;
+    float score;
+    public float health;
+    public float JumpForce;
+    BomberPassed bomberAlert;
+
+    [SerializeField] GameObject DeathScreen;
+    public GameObject bomber;
+    //bool isGrounded = false;
+    bool isAlive = true;
+
+    Rigidbody2D RB;
+    public Text ScoreTxt;
+    public Text HealthTxt;
+
+    private void Start()
+    {
+        score = 0;
+    }
+
+    private void Awake()
+    {
+        RB = GetComponent<Rigidbody2D>();
+        bomberAlert = bomber.GetComponent<BomberPassed>();
+    }
+
+        // Update is called once per frame
+        void Update()
+        {
+           Debug.Log(bomberAlert.bomberAtBase);
+
+            HealthTxt.text = "Health : " + health.ToString("F");
+
+
+            if(Input.GetKey(KeyCode.W))
+            {
+
+                RB.AddForce(Vector2.up * JumpForce);
+
+
+            }
+
+            if(Input.GetKey(KeyCode.S))
+            {
+
+                RB.AddForce(Vector2.down * JumpForce);
+
+
+            }
+
+
+            if(Input.GetKey(KeyCode.D))
+            {
+                RB.AddForce(Vector2.right * RunSpeed);
+            }
+
+            if(Input.GetKey(KeyCode.A))
+            {
+                RB.AddForce(Vector2.left * RunSpeed);
+            }
+
+            if(isAlive)
+            {
+                score += Time.deltaTime * 4;
+                ScoreTxt.text = "SCORE : " + score.ToString("F");
+
+            }
+            if (bomberAlert.bomberAtBase >= 5)
+            {
+                isAlive = false;
+            }
+
+    }
+
+        private void OnCollisionEnter2D(Collision2D collision)
+        {
+
+        if (collision.gameObject.CompareTag("Bomber"))
+        {
+            health -= 5;
+
+        }
+
+        if (collision.gameObject.CompareTag("spike"))
+        {
+             health -= 5;
+
+        }
+        if(collision.gameObject.CompareTag("AAABullet"))
+        {
+             health -= 1;
+
+        }
+
+        if (health <= 0)
+        {
+            isAlive = false;
+    
+
+        }
+        if (isAlive == false)
+        {
+            
+            DeathScreen.SetActive(true);
+            Time.timeScale = 0f;
+            Debug.Log("Dead");
+
+
+        }
+
+    }
+
+
+}
+
